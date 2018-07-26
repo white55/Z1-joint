1d9f17516eb4026bec4ce47745a86a0905101a9a
zhangchao <2472461792@qq.com>
2018-07-26 17:21:37 +0800

    Initial commit

---
+++ src/main/java/com/joint/Z1JointApplication.java
@@ -0,0 +1,12 @@
+package com.joint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z1JointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z1JointApplication.class, args);
+	}
+}
