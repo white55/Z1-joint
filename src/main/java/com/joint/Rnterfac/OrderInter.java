978ece061575d332c7fc3461d35ec8ebb0a93d72
zhangchao <2472461792@qq.com>
2018-07-28 20:26:10 +0800

    及时关闭连接

--- src/main/java/com/joint/Rnterfac/OrderInter.java
+++ src/main/java/com/joint/Rnterfac/OrderInter.java
@@ -69,6 +69,7 @@ public class OrderInter  implements CommandLineRunner {
         } catch (IOException e) {
             e.printStackTrace();
         }finally{
+            //及时关闭连接
             httpClient.getConnectionManager().shutdown();
         }
     }
@@ -104,6 +105,7 @@ public class OrderInter  implements CommandLineRunner {
         } catch (IOException e) {
             e.printStackTrace();
         }finally{
+            //及时关闭连接
             httpClient.getConnectionManager().shutdown();
         }
 
